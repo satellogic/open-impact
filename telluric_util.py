f513cffe028f50d329a5966e03cb1535ab39ceae
Bruno Sanchez-Andrade Nuño <brunosan@satellogic.com>
2018-05-02 16:44:23 +0200

    add bundle download

--- telluric_util.py
+++ telluric_util.py
@@ -62,9 +62,9 @@ def download_scene_iter(sceneset_id,token,data_dir):
         raise ValueError("Telluric search returned %i scenesets, expected 1."%r["count"])
 
     scene_id=r['results'][0]["scene_id"]
-    folder=data_dir+sceneset_id+"/"+scene_id+"/"
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+    folder=data_dir+sceneset_id+"/"+scene_id+"/"+"attachments"+"/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     print("Checking for %i metadata files:"%(len(r['results'][0]['attachments'])))
     for file in r['results'][0]['attachments']:
         print(file['file_name'])
@@ -75,6 +75,9 @@ def download_scene_iter(sceneset_id,token,data_dir):
             with open(filename, 'wb') as fd:
                 for chunk in r.iter_content(chunk_size=128):
                     fd.write(chunk)
+    folder=data_dir+sceneset_id+"/"+scene_id+"/"+"rasters"+"/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     print("Checking for %i rasters files:"%(len(r['results'][0]['rasters'])))
     for file in r['results'][0]['rasters']:
         print(file['file_name'])
@@ -85,3 +88,57 @@ def download_scene_iter(sceneset_id,token,data_dir):
             with open(filename, 'wb') as fd:
                 for chunk in r.iter_content(chunk_size=128):
                     fd.write(chunk)
+def download_scene(sceneset_id,token,data_dir):
+    """
+    Given a set_id, and a token, download all the metadata and rasters
+    files, by requesting a zipped bundle to the server, and downloading it.
+    This is similar to `download_scene_set` but it downloads all the files at once using compression
+    """
+    import time
+    url = 'https://telluric.satellogic.com/v2/scenes/download/'
+
+    header = {'authorization': token}
+    data = {'scene_id': sceneset_id,
+        'async': 1}
+    print("Requesting download...")
+    r = requests.get(url, params=data, headers=header)
+    if r.status_code != 200:
+        raise ValueError("Telluric response error: %s" % r.text)
+    response=requests.get(r.json()['status_path'], headers=header).json()
+
+    #wait for server to finish
+    while response['status']=='Creating':
+        time.sleep(5)
+        response=requests.get(r.json()['status_path'], headers=header).json()
+        print("\r%s: %2.1f%% "%(response['status'],response['progress']),end="")
+    print('. Ready to download.')
+    url = response['download_url']
+    filename = response['filename']
+    header = {'authorization': token}
+
+    r = requests.get(url, headers=header, stream=True)
+    if r.status_code != 200:
+        raise ValueError("Telluric response error: %s" % r.text)
+
+    r_size=float(requests.get(url, stream=True).headers['Content-length'])
+    chunk_size=128
+    chunks=r_size/chunk_size
+    with open(filename, 'wb') as fd:
+        i=0.
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            i+=1
+            print("\r Downloading zipped file: %2.2f%%"%(i/chunks*100), end=" ")
+            fd.write(chunk)
+    print("Done.")
+
+    #unzip
+    import os
+    from zipfile import ZipFile
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    print("Extracting...")
+    with ZipFile(filename, 'r') as fp:
+        fp.extractall(data_dir)
+    print("Done.")
